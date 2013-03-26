dcd2eb092db9b25906860148251dc523be82a072
Joshua Fajardo-Frink <joshua-fajardo-frink@uiowa.edu>
2013-03-25 17:06:45 -0700

    Created new xcode project

---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 3/25/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 3/25/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
