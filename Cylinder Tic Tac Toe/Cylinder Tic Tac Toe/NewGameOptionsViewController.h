4a20826f3803dcbb75a4207e688d64868970f9e2
Joshua Fajardo-Frink <joshua-fajardo-frink@uiowa.edu>
2013-04-08 17:45:29 -0700

    added opponent selection screen and corresponding controller

---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/NewGameOptionsViewController.h
@@ -0,0 +1,18 @@
+//
+//  NewGameOptionsViewController.h
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 4/8/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewGameOptionsViewController : UIViewController 
+{
+    IBOutlet UISegmentedControl *segmentedControl;
+}
+
+
+@end
+
---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/NewGameOptionsViewController.m
@@ -0,0 +1,33 @@
+//
+//  NewGameOptionsViewController.m
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 4/8/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import "NewGameOptionsViewController.h"
+
+@interface NewGameOptionsViewController ()
+
+@end
+
+@implementation NewGameOptionsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+- (void)viewDidUnload{
+    
+}
+
+@end
