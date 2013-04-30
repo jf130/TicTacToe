95f0eba25682309b221c326037ff22d0c641a0db
Joshua Fajardo-Frink <joshua-fajardo-frink@uiowa.edu>
2013-04-30 09:22:32 -0700

    Added VC for displaying How To Play and created instructions PDF

---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/HowToPlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  HowToPlayViewController.h
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 4/30/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HowToPlayViewController : UIViewController
+
+@end
---
+++ Cylinder Tic Tac Toe/Cylinder Tic Tac Toe/HowToPlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  HowToPlayViewController.m
+//  Cylinder Tic Tac Toe
+//
+//  Created by uicsi7 on 4/30/13.
+//  Copyright (c) 2013 uicsi7. All rights reserved.
+//
+
+#import "HowToPlayViewController.h"
+
+@interface HowToPlayViewController ()
+
+@end
+
+@implementation HowToPlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
